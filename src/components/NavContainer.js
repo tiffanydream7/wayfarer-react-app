fb16feabf7bb68b179fad920a64256e0071d08aa
tiffanydream7 <fenniemomoko@gmail.com>
2019-05-04 12:24:02 -0700

    github cleanup

--- src/components/NavContainer.js
+++ src/components/NavContainer.js
@@ -2,16 +2,19 @@ import React, { Component } from "react";
 import Sidebar from "./Sidebar";
 import Profile from "./Profile";
 import { Container, Row, Col, Cards, Button } from 'react-bootstrap';
+import Example from './Modal/Modal';
+
 
     class NavContainer extends Component {
         render() {
             return (
-                // <Container className="">
-                <Row>
-                  <Col><Sidebar /></Col>
-                  <Col xs={8}><Profile /></Col>
-                </Row>
-              // </Container>
+            <Row className='signin-bar'>
+            <Col></Col><Example form='Login' />
+            <Col></Col><Example form='Sign up' />
+            <Col></Col><Example form='Logout' /> 
+            </Row>
+
+
             );
         }
     }
--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -3,23 +3,19 @@ import Post from "./Post";
 import { Container, Row, Col, Button, Collapse, ListGroup } from 'react-bootstrap';
 
 
-
     class PostContainer extends Component {
         colors = [ 'success', 'danger', 'warning', 'info', 'light', 'dark']
 
         render(){
             let posts;
-            // let keyNum = 0;
             if(this.props.info.length){
                 posts = this.props.info.map((post, key) => {
-                    // keyNum++
                     return (
                         <section key={key}>
                             <ListGroup >
                                 <Post
                                     post={post}
                                     color={this.colors[key]}
-                                    // key={keyNum}
                                 />
                             </ListGroup>
                         </section>
